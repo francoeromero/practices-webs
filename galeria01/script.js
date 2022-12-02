709e3a8376c6713f863657d936405e1fac1d9dd9
franromeroe <franromeroeze@gmail.com>
2022-12-02 15:26:23 -0300

    practice

--- galeria01/script.js
+++ galeria01/script.js
@@ -1,12 +1,13 @@
 const fulImgBox = document.getElementById("fulImgBox");
 fulImgBox = document.getElementById("fulImg");
 
-
+//abri la imagen que se selecciono en grande
 function openFulImg(reference){
     fulImgBox.style.display = "flex";
     fulImg.src = reference
 }
 
+//desparecer la imagen grande
 function closeImg() {
     fulImgBox.style.display = "none";  //para que desaparesca la imagen grande seleccionada
 }
